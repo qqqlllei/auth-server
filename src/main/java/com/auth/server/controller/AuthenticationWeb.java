89459c099c3740f2f13bd9c1b827e346ba34285f
lilei <861971291@qq.com>
2018-11-07 11:35:14 +0800

    add password login

--- src/main/java/com/auth/server/controller/AuthenticationWeb.java
+++ src/main/java/com/auth/server/controller/AuthenticationWeb.java
@@ -15,7 +15,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.text.SimpleDateFormat;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
